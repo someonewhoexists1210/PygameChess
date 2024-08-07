71b3aabc681246ee2dcc8f5328dd78448e0c4480
someonewhoexists1210 <130904011+someonewhoexists1210@users.noreply.github.com>
2024-08-07 15:33:51 +0530

    notation

--- main.py
+++ main.py
@@ -101,12 +101,12 @@ WQUEEN = pygame.transform.scale(pygame.image.load('imgs/wqueen.png'), (50,50))
 WKING = pygame.transform.scale(pygame.image.load('imgs/wking.png'), (50,50))
 
 IMGS = {
-        'pawn': (WPAWN, BPAWN),
-        'knight': (WKNIGHT, BKNIGHT),
-        'bishop': (WBISHOP, BBISHOP),
-        'rook': (WROOK, BROOK),
-        'queen': (WQUEEN, BQUEEN),
-        'king': (WKING, BKING),
+        'pawn': (pygame.transform.scale(pygame.image.load('imgs/wpawn.png'), (50,50)), pygame.transform.scale(pygame.image.load('imgs/blpawn.png'), (50,50))),
+        'knight': (pygame.transform.scale(pygame.image.load('imgs/wknight.png'), (50,50)), pygame.transform.scale(pygame.image.load('imgs/blknight.png'), (50,50))),
+        'bishop': (pygame.transform.scale(pygame.image.load('imgs/wbishop.png'), (50,50)), pygame.transform.scale(pygame.image.load('imgs/blbishop.png'), (50,50))),
+        'rook': (pygame.transform.scale(pygame.image.load('imgs/wrook.png'), (50,50)), pygame.transform.scale(pygame.image.load('imgs/blrook.png'), (50,50))),
+        'queen': (pygame.transform.scale(pygame.image.load('imgs/wqueen.png'), (50,50)), pygame.transform.scale(pygame.image.load('imgs/blqueen.png'), (50,50))),
+        'king': (pygame.transform.scale(pygame.image.load('imgs/wking.png'), (50,50)), pygame.transform.scale(pygame.image.load('imgs/blking.png'), (50,50))),
     }
 
 #Misc Variables
@@ -115,6 +115,9 @@ clicked_on_piece = None
 pawnpushed = 0
 piece_taken = 0
 whites_turn = True
+movesdone = 0
+positions = []
+file = open('game.txt', 'w+')
 
 #Useful lambdas
 right = lambda x: abcs[abcs.index(x[0]) + 1] + x[1]
@@ -606,6 +609,13 @@ def is_sufficient(color = None):
         if len(pss[color]) == 2:
             return False
 
+#Gets current position
+def get_position():
+    temp = []
+    for p in pieces:
+        temp.append(p.square)
+    return temp
+
 #Checks for a draw
 def checkdraw():
     if len(pieces) <= 4:
@@ -613,18 +623,22 @@ def checkdraw():
 
         for p in pieces:
             if type(p) is queen or type(p) is rook or type(p) is pawn:
-                if pawnpushed < 50 or piece_taken < 50: return False, 'Not a draw'     
-                else: return True, '50 move rule'
+                if pawnpushed >= 50 and piece_taken >= 50: return True, '50 move rule'    
+                elif positions.count(get_position()) >= 2: return True, 'Repetition'
+                else: return False, 'Not a draw' 
                     
         else:
             if len(wpieces) <= 2 and len(bpieces) <= 2: return True, 'Insufficient Material'            
-            return False, 'Not a draw'
+            elif positions.count(get_position()) >= 2: return True, 'Repetition'
+            else: return False, 'Not a draw'
+
             
     else:
-        if pawnpushed < 50 or piece_taken < 50: return False, 'Not a draw'
-        else: return True, '50 move rule'      
-
-#Checks
+        if pawnpushed >= 50 and piece_taken >= 50: return True, '50 Move Rule'
+        elif positions.count(get_position()) >= 2: return True, 'Repetition'
+        else: return False, 'Not a draw'
+      
+#Checks if in check
 def checkforchecks():
     wking.in_check = False
     bking.in_check = False
@@ -698,13 +712,18 @@ def setlegalmoves(sidechecked):
 
 #Checks if move has been made and moves the chosen piece
 def checkmove(pos):
-    global clicked_on_piece, whites_turn, piece_taken, pawnpushed, last_move
+    global clicked_on_piece, whites_turn, piece_taken, pawnpushed, last_move, movesdone
 
     def movechange():
-        global whites_turn, clicked_on_piece, pawnpushed
+        global whites_turn, clicked_on_piece, pawnpushed, movesdone
         whites_turn = not whites_turn
         clicked_on_piece = None
         pawnpushed += 0.5
+        movesdone += 0.5
+        for p in pieces:
+            p.check_moves()
+        positions.append(get_position())
+        notation()
 
     for mv in clicked_on_piece.moves:
         if clicked_on_piece == wking:
@@ -822,9 +841,33 @@ def castles(color):
     if result[1]:
         selfking.moves.add(castlesqs[color]['qside'][1] + "p")
         
+#Writes Notation
+def notation():
+    checkforchecks()
+    if last_move == None: return
+
+    x = last_move[1]
+    
+    if type(x) is tuple:
+        x = "x" + x[0]
+    if bking.in_check or wking.in_check:
+        x = x + '+'
+
+
+    if type(last_move[0]) is knight:
+        x = 'N' + x
+    elif type(last_move[0]) is pawn:
+        if x[0] == 'x':
+            x = last_move[2][0] + x
+    else:
+        if 'O' not in x:
+            x = type(last_move[0]).__name__[0].upper() + x
+
+    file.write(x + " ")
+    
 # Main game loop
 def main():
-    global wpieces, wtime, bpieces, wking, pieces, wpieces, bpieces, bking, run, wkrook, wqrook, bkrook, bqrook, last_move
+    global wpieces, wtime, bpieces, wking, pieces, wpieces, bpieces, bking, run, wkrook, wqrook, bkrook, bqrook, last_move, draw
     run = True
     clock = pygame.time.Clock()
     resignblack = Button(450, 50, "Resign", (0, 0, 0),(255, 255, 255), False, False, 150, 40)
@@ -982,14 +1025,28 @@ def main():
         pygame.display.update()
     
 def result(res):
-    global run
+    global run, file
+
     while True:
         message = Button(225, 175, res, (255, 255, 255), 100, 50)
         message.draw()
+        
+
         run = False
 
         for event in pygame.event.get():
                 if event.type==pygame.QUIT:
+                    file.close()
+                    f = open('game.txt', 'r+')
+                    pgn = f.read()
+                    f1 = open('game.txt', 'w+')
+                    if res.split()[-1] == "CHECKMATE":
+                        f1.write(pgn[:-2] + '#')
+                    elif draw[0]:
+                        if pgn[-2] != "+":
+                            f1.write(pgn[:-1] + '=')
+                        else:
+                            f1.write(pgn[:-2] + '=')
                     pygame.quit()
                     sys.exit() 
         pygame.display.update()
